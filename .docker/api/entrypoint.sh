495bf66c69b5fce4963f13e4c5259ef192d7a0b5
Jacques Ramondot neto <jramondo@c3r7p4.42sp.org.br>
2024-10-23 18:21:55 -0300

    Subindo o projeto

---
+++ .docker/api/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Create logs directory
+mkdir -p logs
+
+# Generate SSL certificate
+openssl req -x509 -newkey rsa:4096 \
+    -keyout ./certs/localhost.key -out ./certs/localhost.crt \
+    -days 365 -nodes -subj "/C=BR/ST=SP/L=São Paulo/O=42/OU=42/CN=transcendence.42.fr/UID=transcendence"
+
+# Create translation files
+echo "Creating Tranlation Files"
+python manage.py makemessages --all
+
+# Apply database migrations
+echo "Apply database migrations"
+python manage.py migrate
+
+# Start server
+echo "Starting server"
+python manage.py runserver 0.0.0.0:8000
